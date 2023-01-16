7c904b5e69c995ac295acf5811ec02adb0c82070
ashishingle29 <ashishingle589@gmail.com>
2023-01-16 05:51:56 +0530

    All Done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import IncrementDecrement from './Increment-Decrement';
-import UpperLowerCase from './Upper-LowerCase/index';
-import RegistartionForm from './Registartion-Form/index';
-import TopHeader from './TopHeader/index'
+import Quizcomponent from './OnlineExam/QuizComp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TopHeader />
-    <IncrementDecrement />
-    <UpperLowerCase />
-    <RegistartionForm />
+   <Quizcomponent/>
   </React.StrictMode>
 );
